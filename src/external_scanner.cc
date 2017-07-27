c80cbb97158d106722a18fd73e715826c35a6b69
Jiri Sadek <jiri.sadek@nic.cz>
2017-07-27 16:11:18 +0200

    Ticket #19200 - Subprocess class

--- src/external_scanner.cc
+++ src/external_scanner.cc
@@ -1,23 +1,19 @@
-#include <map>
-#include <array>
 #include <iostream>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <unordered_set>
 #include <boost/lexical_cast.hpp>
-#include <boost/algorithm/string/join.hpp>
 
 #include "src/utils.hh"
+#include "src/subprocess.hh"
 #include "src/external_scanner.hh"
 #include "src/cdnskey_scanner_impl/input_serializer.hh"
-#include "src/cdnskey_scanner_impl/output_parsert.hh"
+#include "src/cdnskey_scanner_impl/output_parser.hh"
 #include "src/log.hh"
+
 
 namespace Fred {
 namespace Akm {
 
 namespace {
-
 
     unsigned long long compute_best_runtime_for_input(
         const NameserverDomainsCollection& _tasks,
@@ -37,7 +33,6 @@ namespace {
         );
         return runtime;
     }
-
 
 }
 
@@ -68,186 +63,104 @@ void ExternalScannerTool::scan(const NameserverDomainsCollection& _tasks, OnResu
     // subprocess_argv.push_back(runtime_arg.c_str());
     subprocess_argv.push_back(nullptr);
 
+    Subprocess scanner_subprocess(subprocess_argv);
+    log()->debug("parent_pid={} child_pid={}", scanner_subprocess.parent_pid(), scanner_subprocess.child_pid());
 
-    struct ParentPipe
+    auto scanner_writter = [&scanner_subprocess](const std::string& _str)
     {
-        enum Enum
-        {
-            WRITE = 0,
-            READ = 1
-        };
-    };
-    struct Descriptor
-    {
-        enum Enum
-        {
-            READ = 0,
-            WRITE = 1
-        };
+        scanner_subprocess.write(_str);
     };
 
-    int pipes[2][2];
-    if (const int ret = pipe(pipes[ParentPipe::READ]) != 0)
+    auto stdout_writter = [](const std::string& _str)
     {
-        throw std::runtime_error("unable to spawn subprocess (pipe construct failed)");
-    }
-    if (const int ret = pipe(pipes[ParentPipe::WRITE]) != 0)
-    {
-        throw std::runtime_error("unable to spawn subprocess (pipe construct failed)");
-    }
-
-    int& parent_rd_fd = pipes[ParentPipe::READ][Descriptor::READ];
-    int& parent_wr_fd = pipes[ParentPipe::WRITE][Descriptor::WRITE];
-    int& child_rd_fd = pipes[ParentPipe::WRITE][Descriptor::READ];
-    int& child_wr_fd = pipes[ParentPipe::READ][Descriptor::WRITE];
-
-    const pid_t parent_pid = getpid();
-    const pid_t child_pid = fork();
-    if (child_pid == -1)
-    {
-        throw std::runtime_error("running subprocess failed (fork() failed)");
-    }
-    else if (child_pid == 0)
-    {
-        /* child */
-        dup2(child_rd_fd, STDIN_FILENO);
-        dup2(child_wr_fd, STDOUT_FILENO);
-
-        close(child_rd_fd);
-        close(child_wr_fd);
-        close(parent_rd_fd);
-        close(parent_wr_fd);
-
-        ::execv(subprocess_argv[0], const_cast<char **>(&subprocess_argv[0]));
-        throw std::runtime_error("external scanner tool subprocess call failed");
-    }
-    else
-    {
-        log()->debug("parent_pid={} child_pid={}", parent_pid, child_pid);
-
-        close(child_rd_fd);
-        close(child_wr_fd);
-
-        auto write_to_scanner = [&parent_wr_fd](const std::string& _str)
+        if (_str.size() > 0)
         {
-            if (_str.size() > 0)
-            {
-                const ssize_t ret = write(parent_wr_fd, _str.c_str(), _str.size());
-                if (ret == -1)
-                {
-                    throw std::runtime_error("write to pipe failed");
-                }
-                else if (ret != _str.size())
-                {
-                    throw std::runtime_error("write to pipe incomplete?");
-                }
-            }
-        };
-
-        auto write_to_stdout = [](const std::string& _str)
-        {
-            if (_str.size() > 0)
-            {
-                std::cout << _str;
-            }
-        };
-
-        ScanTaskSerializer serializer;
-        serializer.serialize_insecure(_tasks, write_to_scanner);
-        serializer.serialize_secure(_tasks, write_to_scanner);
-
-        close(parent_wr_fd);
-        log()->info("total domains for scan sent: {} (insecure={} secure={})",
-            serializer.get_insecure_domains_counter() + serializer.get_secure_domains_counter(),
-            serializer.get_insecure_domains_counter(),
-            serializer.get_secure_domains_counter()
-        );
-
-        /* TODO: to normal function */
-        ScanResultParser scan_result_parser;
-        auto buffer_results = [&scan_result_parser](std::string& _raw_buffer, std::vector<ScanResult>& _result_buffer)
-        {
-            const auto newline_ptr = std::find(_raw_buffer.begin(), _raw_buffer.end(), '\n');
-            if (newline_ptr != _raw_buffer.end())
-            {
-                const auto result_line = std::string(_raw_buffer.begin(), newline_ptr);
-                _raw_buffer.erase(_raw_buffer.begin(), newline_ptr + 1);
-                try
-                {
-                    _result_buffer.emplace_back(scan_result_parser.parse(result_line));
-                }
-                catch (const std::exception& ex)
-                {
-                    log()->error("buffer[!]: {}", result_line);
-                    log()->error("buffer[!]: {}", ex.what());
-                }
-                return true;
-            }
-            return false;
-        };
-
-        long total_results = 0;
-
-        const auto RESULT_BUFFER_ITEM_SIZE = 1024;
-        std::vector<ScanResult> result_buffer;
-        result_buffer.reserve(RESULT_BUFFER_ITEM_SIZE);
-
-        const auto READ_CHUNK_SIZE = 512;
-        std::array<char, READ_CHUNK_SIZE> chunk;
-        std::string raw_buffer;
-
-        log()->debug("result buffer size: {} item(s)", RESULT_BUFFER_ITEM_SIZE);
-        log()->debug("read chunk buffer size: {} [b]", READ_CHUNK_SIZE);
-        log()->info("waiting for results...");
-        int read_count = 0;
-        int child_status;
-        while ((read_count = read(parent_rd_fd, chunk.data(), chunk.size() - 1)) > 0)
-        {
-            if (read_count >= 0)
-            {
-                chunk[read_count] = 0;
-                raw_buffer += std::string(chunk.data());
-                chunk = {{}};
-                while (buffer_results(raw_buffer, result_buffer))
-                {
-                    if (result_buffer.size() == result_buffer.capacity())
-                    {
-                        _callback(result_buffer);
-                        total_results += result_buffer.size();
-                        log()->debug("buffer[-]: buffer full - saving results (saved={} total={})",
-                            result_buffer.size(), total_results
-                        );
-                        result_buffer.clear();
-                    }
-                }
-            }
-            else
-            {
-                throw std::runtime_error("IOError");
-            }
+            std::cout << _str;
         }
-        if (result_buffer.size())
+    };
+
+    ScanTaskSerializer serializer;
+    serializer.serialize_insecure(_tasks, scanner_writter);
+    serializer.serialize_secure(_tasks, scanner_writter);
+
+    scanner_subprocess.write_eof();
+
+    log()->info("total domains for scan sent: {} (insecure={} secure={})",
+        serializer.get_insecure_domains_counter() + serializer.get_secure_domains_counter(),
+        serializer.get_insecure_domains_counter(),
+        serializer.get_secure_domains_counter()
+    );
+
+    /* TODO: to normal function */
+    ScanResultParser scan_result_parser;
+    auto buffer_results = [&scan_result_parser](std::string& _raw_buffer, std::vector<ScanResult>& _result_buffer)
+    {
+        const auto newline_ptr = std::find(_raw_buffer.begin(), _raw_buffer.end(), '\n');
+        if (newline_ptr != _raw_buffer.end())
         {
-            _callback(result_buffer);
-            total_results += result_buffer.size();
-        }
-        const pid_t exited_child = waitpid(child_pid, &child_status, 0);
-        log()->debug("child exit-status:{}", WEXITSTATUS(child_status));
-        if (exited_child == child_pid)
-        {
-            const bool child_exited_successfully = WIFEXITED(child_status) && (WEXITSTATUS(child_status) == EXIT_SUCCESS);
-            if (!child_exited_successfully)
+            const auto result_line = std::string(_raw_buffer.begin(), newline_ptr);
+            _raw_buffer.erase(_raw_buffer.begin(), newline_ptr + 1);
+            try
             {
-                throw std::runtime_error("child terminated unsuccesfully :(");
+                _result_buffer.emplace_back(scan_result_parser.parse(result_line));
+            }
+            catch (const std::exception& ex)
+            {
+                log()->error("buffer[!]: {}", result_line);
+                log()->error("buffer[!]: {}", ex.what());
+            }
+            return true;
+        }
+        return false;
+    };
+
+    long total_results = 0;
+
+    const auto RESULT_BUFFER_ITEM_SIZE = 1024;
+    std::vector<ScanResult> result_buffer;
+    result_buffer.reserve(RESULT_BUFFER_ITEM_SIZE);
+
+    Subprocess::ReadBuffer chunk;
+    std::string raw_buffer;
+
+    log()->debug("result buffer size: {} item(s)", RESULT_BUFFER_ITEM_SIZE);
+    log()->info("waiting for results...");
+    int read_count = 0;
+    int child_status;
+    while ((read_count = scanner_subprocess.read(chunk)) > 0)
+    {
+        if (read_count >= 0)
+        {
+            chunk[read_count] = 0;
+            raw_buffer += std::string(chunk.data());
+            chunk = {{}};
+            while (buffer_results(raw_buffer, result_buffer))
+            {
+                if (result_buffer.size() == result_buffer.capacity())
+                {
+                    _callback(result_buffer);
+                    total_results += result_buffer.size();
+                    log()->debug("buffer[-]: buffer full - saving results (saved={} total={})",
+                        result_buffer.size(), total_results
+                    );
+                    result_buffer.clear();
+                }
             }
         }
         else
         {
-            throw std::runtime_error("waitpid failure (" + std::string(std::strerror(errno)) + ")");
+            throw std::runtime_error("IOError");
         }
-
-        log()->info("total proccessed results: {}", total_results);
     }
+    if (result_buffer.size())
+    {
+        _callback(result_buffer);
+        total_results += result_buffer.size();
+    }
+
+    const int child_exit_status = scanner_subprocess.wait();
+    log()->debug("child exit-status:{}", child_exit_status);
+    log()->info("total proccessed results: {}", total_results);
 }
 
 
---
+++ src/subprocess.hh
@@ -0,0 +1,162 @@
+#ifndef SUBPROCESS_HH_91D7AE444630995FCAD6ED285B6170FD//date "+%s" | md5sum | cut -f1 -d" " | tr "[a-f]" "[A-F]" | tr -d "\n"
+#define SUBPROCESS_HH_91D7AE444630995FCAD6ED285B6170FD
+
+#include <map>
+#include <array>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+namespace Fred {
+namespace Akm {
+
+
+struct Descriptor
+{
+    enum Enum
+    {
+        READ = 0,
+        WRITE = 1
+    };
+};
+
+class Pipe
+{
+private:
+    int fd_[2];
+
+public:
+    Pipe()
+    {
+        if (const int ret = pipe(fd_) != 0)
+        {
+            throw std::runtime_error("create of pipe failed (ret=" + std::to_string(ret) + ")");
+        }
+    }
+
+    ~Pipe()
+    {
+        close();
+    }
+
+    const int read_fd() const
+    {
+        return fd_[Descriptor::READ];
+    }
+
+    const int write_fd() const
+    {
+        return fd_[Descriptor::WRITE];
+    }
+
+    void close()
+    {
+        ::close(fd_[Descriptor::READ]);
+        ::close(fd_[Descriptor::WRITE]);
+    }
+};
+
+
+class Subprocess
+{
+private:
+    Pipe write_pipe_;
+    Pipe read_pipe_;
+    pid_t parent_pid_ = -1;
+    pid_t child_pid_ = -1;
+
+public:
+    Subprocess(const std::vector<const char*>& _argv)
+    {
+        parent_pid_ = ::getpid();
+        child_pid_ = ::fork();
+
+        if (child_pid_ == -1)
+        {
+            throw std::runtime_error("running subprocess failed (fork() failed)");
+        }
+        else if (child_pid_ == 0)
+        {
+            /* child */
+            ::dup2(write_pipe_.read_fd(), STDIN_FILENO);
+            ::dup2(read_pipe_.write_fd(), STDOUT_FILENO);
+
+            write_pipe_.close();
+            read_pipe_.close();
+
+            ::execv(_argv[0], const_cast<char **>(&_argv[0]));
+            throw std::runtime_error("subprocess call failed");
+        }
+        else
+        {
+            ::close(write_pipe_.read_fd());
+            ::close(read_pipe_.write_fd());
+        }
+    }
+
+    const pid_t parent_pid() const
+    {
+        return parent_pid_;
+    }
+
+    const pid_t child_pid() const
+    {
+        return child_pid_;
+    }
+
+    int wait()
+    {
+        int child_status;
+        const pid_t exited_child = waitpid(child_pid_, &child_status, 0);
+        if (exited_child == child_pid_)
+        {
+            const bool child_exited_successfully = WIFEXITED(child_status) && (WEXITSTATUS(child_status) == EXIT_SUCCESS);
+            if (!child_exited_successfully)
+            {
+                throw std::runtime_error("child terminated unsuccesfully :(");
+            }
+        }
+        else
+        {
+            throw std::runtime_error("waitpid failure (" + std::string(std::strerror(errno)) + ")");
+        }
+        return WEXITSTATUS(child_status);
+    }
+
+    ssize_t write(const std::string& _buffer)
+    {
+        if (_buffer.size() > 0)
+        {
+            const ssize_t ret = ::write(write_pipe_.write_fd(), _buffer.c_str(), _buffer.size());
+            if (ret == -1)
+            {
+                throw std::runtime_error("write to pipe failed");
+            }
+            else if (ret != _buffer.size())
+            {
+                throw std::runtime_error("write to pipe incomplete?");
+            }
+            return ret;
+        }
+        return 0;
+    }
+
+    void write_eof()
+    {
+        ::close(write_pipe_.write_fd());
+    }
+
+    typedef std::array<char, 1024> ReadBuffer;
+
+    int read(ReadBuffer& _buffer)
+    {
+        return ::read(read_pipe_.read_fd(), _buffer.data(), _buffer.size() - 1);
+    }
+
+};
+
+
+} // namespace Akm
+} // namespace Fred
+
+#endif//SUBPROCESS_HH_91D7AE444630995FCAD6ED285B6170FD
