3bbcf37c602f739f9bba6ffcb3fc0a80ef8b1ef7
Jiri Sadek <jiri.sadek@nic.cz>
2017-07-27 15:43:17 +0200

    Ticket #19200 - Move scanner output (result) parser to separate file

---
+++ src/cdnskey_scanner_impl/output_parser.hh
@@ -0,0 +1,23 @@
+#ifndef OUTPUT_PARSER_HH_DDEC7B4468DE682C3826F9CD09E6C08B//date "+%s" | md5sum | cut -f1 -d" " | tr "[a-f]" "[A-F]" | tr -d "\n"
+#define OUTPUT_PARSER_HH_DDEC7B4468DE682C3826F9CD09E6C08B
+
+#include "src/i_scanner.hh"
+
+#include <string>
+
+
+namespace Fred {
+namespace Akm {
+
+
+class ScanResultParser
+{
+public:
+    ScanResult parse(const std::string& _line) const;
+};
+
+
+} // namespace Akm
+} // namespace Fred
+
+#endif//OUTPUT_PARSER_HH_DDEC7B4468DE682C3826F9CD09E6C08B
