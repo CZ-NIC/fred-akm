4463e638d4271d7ad2ca0dc3919cc571c1978a98
Miroslav Franc <miroslav.franc@nic.cz>
2018-12-07 16:44:43 +0100

    Ticket #21315 - fix licenses

--- contrib/fedora/fred-akm.spec
+++ contrib/fedora/fred-akm.spec
@@ -3,7 +3,7 @@ Version:    %{our_version}
 Release:    %{?our_release}%{!?our_release:1}%{?dist}
 Summary:	FRED - automated keyset management client
 Group:		Applications/Utils
-License:	GPL
+License:	GPLv3+
 URL:		http://fred.nic.cz
 Source0:	%{name}-%{version}.tar.gz
 Source1:    idl-%{idl_branch}.tar.gz
--- src/i_mailer.hh
+++ src/i_mailer.hh
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2017  CZ.NIC, z. s. p. o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef I_MAILER_HH_6D59404C3B72544DBC5990EF3624B613//date "+%s"|md5sum|tr "[a-f]" "[A-F]"
 #define I_MAILER_HH_6D59404C3B72544DBC5990EF3624B613
 
